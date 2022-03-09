deb6f846ae94733c68204cecac56c898831cad97
NghiaNim <72574304+NghiaNim@users.noreply.github.com>
2022-03-10 01:19:01 +0400

    added cool animation and stuff!!!

---
+++ loyalty_test/Assets/Scripts/boxThrow.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class boxThrow : MonoBehaviour
+{
+    public Rigidbody rb;
+    public float strength = 2000;
+    public XRGrabInteractable box;
+    public void Throw()
+    {
+        Vector3 direction = box.GetOldestInteractorSelecting().transform.forward;
+        direction *= strength;
+
+        box.enabled = false;
+
+        rb.AddForce(direction);
+
+        StartCoroutine(reenable());
+
+
+    }
+
+    IEnumerator reenable()
+    {
+        yield return new WaitForSeconds(2);
+
+        box.enabled = true;
+    }
+
+    // Start is called before the first frame update
+}
